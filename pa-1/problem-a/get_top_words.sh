583e1153a2a78d59842c232bb01e978159d7246d
Vishruth Bharath <vbharath@ucsc.edu>
2023-04-12 21:24:47 -0700

    finished part-a

--- pa-1/problem-a/get_top_words.sh
+++ pa-1/problem-a/get_top_words.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-test
+
+# Get the input arguments
+filename=$1
+k=$2
+
+stopwords=($(cat stopwords | tr '\n' ' '))
+
+words=($(cat $filename))
+
+words=($(grep -o -w -i '[[:alpha:]]*' $filename))
+
+words=($(echo "${words[@]}" | tr '[:upper:]' '[:lower:]'))
+
+for stopword in ${stopwords[@]}; do
+    words=($(echo "${words[@]}" | tr ' ' '\n' | grep -vw $stopword | tr '\n' ' '))
+done
+
+declare -A freq
+for word in "${words[@]}"; do
+    freq[$word]=$((freq[$word]+1))
+done
+
+# Sort the words by frequency, then alphabetically if frequencies are equal
+sorted=($(for word in "${!freq[@]}"; do echo "$word ${freq[$word]}"; done | sort -k2,2nr -k1,1 | head -n "$k" | awk '{print $1}'))
+
+# Print the top k most frequent words
+for word in "${sorted[@]}"; do
+    echo "$word"
+done
