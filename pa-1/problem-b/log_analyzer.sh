b5a66cf77abcefd7a942dc471a967ab16219eb95
Vishruth Bharath <vishruthbharath@gmail.com>
2023-04-17 21:48:25 -0700

    redo

--- pa-1/problem-b/log_analyzer.sh
+++ pa-1/problem-b/log_analyzer.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-# Check if input file is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <log_file>"
-    exit 1
-fi
+# Read log file and extract content sizes and response codes
+cs=($(awk '{gsub(/-/, "0"); print $NF}' "$1"))
+rc=($(awk '{print $(NF-1)}' "$1"))
 
-# Read log file and extract relevant columns
-content_sizes=($(awk '{gsub(/-/, "0"); print $NF}' "$1"))
-response_codes=($(awk '{print $(NF-1)}' "$1"))
-
-# Calculate average content size and count unique response codes
-total_size=0
-declare -A codes
-for size in "${content_sizes[@]}"; do
-    total_size=$((total_size+size))
+# Calculate averages and counts
+ts=0
+declare -A c
+for s in "${cs[@]}"; do
+    ts=$((ts+s))
 done
-avg_size=$((total_size/${#content_sizes[@]}))
-for code in "${response_codes[@]}"; do
-    codes[$code]=1
+as=$((ts/${#cs[@]}))
+for cd in "${rc[@]}"; do
+    c[$cd]=1
 done
-num_codes=${#codes[@]}
+nc=${#c[@]}
 
-# Print results
-echo "$avg_size"
-echo "$num_codes"
+# Output results
+echo "$as"
+echo "$nc"
